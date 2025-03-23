b23fedb5c927117e4009fb00b46bb89d7a6ec090
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-03-23 23:40:40 +0530

    done display

--- frontend/src/pages/AddUser.jsx
+++ frontend/src/pages/AddUser.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const AddUser = () => {
+const AddUser = (props) => {
+
+  const {_id, name, email, age, address} = props.user
+
   return (
     <div>
-      <h1>Hi User</h1>
+      
+      <h1>Hi {name}</h1>
+      <p>ID: {_id}</p>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      <p>Age: {age}</p>
+      <p>Address: {address}</p>
+      <br />
+      <button className='bg-red-400'>Update</button>
+      <br />
+      <button className='bg-red-400'>Delete</button>
+      <br />
+      
+      
     </div>
   )
 }
--- frontend/src/pages/UserDetais.jsx
+++ frontend/src/pages/UserDetais.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import AddUser from './AddUser'
 
+const URL = "http://localhost:5000/users";
 
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
 
 const UserDetais = () => {
+
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetchHandler().then((data) => setUsers(data.users));
+  }, []);
+
   return (
     <div >
-        <h1>User Details</h1>
+        <h1>User Details display Page</h1>
+        <br />
+        <div>
+          {users && users.map((user, i ) => (
+            <div key={i}>
+              <AddUser user={user} />
+            </div>
+          
+          ))}
+        </div>
       
     </div>
   )
